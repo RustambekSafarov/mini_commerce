814a5c1ba3330bd828ccd9d7ff7bc757307cea53
RustambekSafarov <devdoyen@gmail.com>
2025-05-02 21:13:54 +0500

    django generic view

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializer import ProductSerializer
 
+class ProductView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 # Create your views here.
